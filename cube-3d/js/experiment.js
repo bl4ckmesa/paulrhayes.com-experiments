c825110b0529f117bf0fdcb300a8839b7643b2a3
bl4ckmesa <robinsonaarond@gmail.com>
2015-05-16 19:36:30 -0700

    At least it populates the cube sides :)

--- cube-3d/js/experiment.js
+++ cube-3d/js/experiment.js
@@ -1,3 +1,5 @@
+var images = {};
+
 var props = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' '),
     prop,
     el = document.createElement('div');
@@ -36,27 +38,53 @@ $('body').keydown(function(evt) {
 });
 
 function populateSides() {
-	document.getElementById('c1_1').innerHTML = '<img src="http://sciencenordic.com/sites/default/files/imagecache/620x/rabbit_0.jpg" style="width: 100%; height: 100%" />'
-	document.getElementById('c1_2').innerHTML = i;
-	document.getElementById('c1_3').innerHTML = "Three";
-	document.getElementById('c1_4').innerHTML = "Four";
-	document.getElementById('c1_5').innerHTML = "Five";
-	document.getElementById('c1_6').innerHTML = "Six";
+	$("#i1_1").attr('src', "images/Transparent.gif");
+	$("#i1_2").attr('src', "images/Transparent.gif");
+	$("#i1_3").attr('src', "images/Transparent.gif");
+	$("#i1_4").attr('src', "images/Transparent.gif");
+	$("#i1_5").attr('src', "images/Transparent.gif");
+	$("#i1_6").attr('src', "images/Transparent.gif");
+
+	$("#img1").attr('src', "images/Transparent.gif");
+	$("#img2").attr('src', "images/Transparent.gif");
+	$("#img3").attr('src', "images/Transparent.gif");
+	$("#img4").attr('src', "images/Transparent.gif");
+	$("#img5").attr('src', "images/Transparent.gif");
+	$("#img6").attr('src', "images/Transparent.gif");
 }
 
-function readURL(input,imgtag) {
+function readURL(input,imgtag,cubesideimg) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
             $(imgtag).attr('src', e.target.result);
+            $(cubesideimg).attr('src', e.target.result);
         }
         reader.readAsDataURL(input.files[0]);
     }
 }
 
-$("#imgside1").change(function(){ readURL(this,'#img1'); });
-$("#imgside2").change(function(){ readURL(this,'#img2'); });
-$("#imgside3").change(function(){ readURL(this,'#img3'); });
-$("#imgside4").change(function(){ readURL(this,'#img4'); });
-$("#imgside5").change(function(){ readURL(this,'#img5'); });
-$("#imgside6").change(function(){ readURL(this,'#img6'); });
+$("#imgside1").change(function(){ readURL(this,'#img1','#i1_1'); });
+$("#imgside2").change(function(){ readURL(this,'#img2','#i1_2'); });
+$("#imgside3").change(function(){ readURL(this,'#img3','#i1_3'); });
+$("#imgside4").change(function(){ readURL(this,'#img4','#i1_4'); });
+$("#imgside5").change(function(){ readURL(this,'#img5','#i1_5'); });
+$("#imgside6").change(function(){ readURL(this,'#img6','#i1_6'); });
+
+
+
+// cropperOptions = {
+// 	uploadUrl:'php/img_save_to_file.php',
+// 	cropUrl:'php/img_crop_to_file.php',
+// 	zoomFactor:10,
+// 	doubleZoomControls:true,
+// 	rotateFactor:10,
+// 	rotateControls:true,
+// 	modal:true
+// }		
+// 		
+// var cropper = new Croppic('croppic1', cropperOptions);
+// cropper.destroy() 	// no need explaining here :) 
+// cropper.reset() 	// destroys and then inits the cropper
+
+populateSides();
